20ca5a28b30de9d68645270551874ba61f9adfaf
Nasru <nasrukalarikkal7@gmail.com>
2023-06-10 14:03:47 +0530

    code

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:mobitechproject/services/api_services.dart';
+import 'package:mobitechproject/view/details.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -13,15 +14,17 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     ApiServices apiServices = ApiServices();
+    // bool isExpended = false;
+    // int currentindex = 0;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
-        leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
-        title: const Text('MWS'),
+        leading: IconButton(onPressed: () {}, icon: const Icon(Icons.menu)),
+        title: const Text('Maybeline'),
         actions: [
           IconButton(
             onPressed: () {},
-            icon: Icon(Icons.search),
+            icon: const Icon(Icons.search),
           ),
         ],
       ),
@@ -32,43 +35,93 @@ class _MyHomePageState extends State<MyHomePage> {
           if (snapshot.hasData) {
             return ListView.builder(
               itemCount: data1!.length,
-              itemBuilder: (context, index) => Card(
-                child: Column(
-                  children: [
-                    Image.network(data1[index].imageLink!),
-                    Text(data1[index].name!),
-                    Text(data1[index].brand!),
-                    Text("₹${data1[index].price!}"),
-                    RatingBar.builder(
-                      initialRating: data1[index].rating == null
-                          ? 0.0
-                          : data1[index].rating!,
-                      allowHalfRating: true,
-                      itemCount: 5,
-                      itemSize: 23,
-                      itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
-                      itemBuilder: (context, _) => Icon(
-                        Icons.star,
-                        color: Colors.amber,
+              itemBuilder: (context, index) => Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Details(
+                            productModel: data1[index],
+                          ),
+                        ));
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      elevation: 8,
+                      child: Column(
+                        children: [
+                          Image.network(data1[index].imageLink!),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              data1[index].name!,
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 17),
+                            ),
+                          ),
+                          Text(
+                            data1[index].brand!,
+                            style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 16,
+                                color: Colors.grey),
+                          ),
+                          Text(
+                            "₹${data1[index].price!},",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w400, fontSize: 19),
+                          ),
+                          RatingBar.builder(
+                            initialRating: data1[index].rating == null
+                                ? 0.0
+                                : data1[index].rating!,
+                            allowHalfRating: true,
+                            itemCount: 5,
+                            itemSize: 23,
+                            itemPadding:
+                                const EdgeInsets.symmetric(horizontal: 4.0),
+                            itemBuilder: (context, _) => const Icon(
+                              Icons.star,
+                              color: Colors.amber,
+                            ),
+                            onRatingUpdate: (rating) {
+                              print(rating);
+                            },
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(0.9),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                SizedBox(
+                                  width: 270,
+                                  child: Text(
+                                    data1[index].description!,
+                                    maxLines: 1,
+                                  ),
+                                ),
+                                ElevatedButton(
+                                    style: const ButtonStyle(
+                                        backgroundColor:
+                                            MaterialStatePropertyAll(
+                                                Colors.grey)),
+                                    onPressed: () {},
+                                    child: const Text('More')),
+                              ],
+                            ),
+                          )
+                        ],
                       ),
-                      onRatingUpdate: (rating) {
-                        print(rating);
-                      },
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                            width: 280, child: Text(data1[index].description!)),
-                        ElevatedButton(onPressed: () {}, child: Text('More'))
-                      ],
-                    )
-                  ],
+                  ),
                 ),
               ),
             );
           } else {
-            return Center(
+            return const Center(
               child: CircularProgressIndicator(),
             );
           }
